821510c55a30a0a4129af3dcfa2d2da031600b8a
langzi-zmg <zmg_2013@163.com>
2019-03-26 19:02:25 +0800

    Create cronCreatePartitions.sh

---
+++ cronCreatePartitions.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+hivePath="/usr/local/service/hive/bin/hive"
+table=(application_activated control_click pageview readingstats share_event)
+
+if [ $# == 2 ]; then
+    datebeg=$1
+    dateend=$2
+else
+    echo "请输入开始时间和结束日期，格式为2017-04-04"
+    exit 1
+fi
+
+beg_s=`date -d "$datebeg" +%s`
+end_s=`date -d "$dateend" +%s`
+echo "处理时间范围：$beg_s 至 $end_s"
+
+echo "create PARTITION........."
+while [ "$beg_s" -le "$end_s" ];do
+    day=`date -d @$beg_s +"%Y-%m-%d"`;
+    beg_s=$((beg_s+86400));
+    for i in ${table[@]}
+    do
+    for j in {0..23}
+    do
+    echo "$hivePath -e 'set hive.execution.engine=mr;use bi;alter table $i DROP  IF NOT EXISTS PARTITION (dt=$day,hour=`printf "%02d\n" $j`)'"
+    done
+    done
+done
+echo "全部处理完成"
