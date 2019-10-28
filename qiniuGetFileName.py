6263ea26eed433f764e618c71416e63c3a493668
langzi-zmg <zmg_2013@163.com>
2019-10-28 18:34:51 +0800

    Update qiniuGetFileName.py

--- qiniuGetFileName.py
+++ qiniuGetFileName.py
@@ -3,15 +3,15 @@ from qiniu import Auth
 from qiniu import BucketManager
 import json
 
-access_key = '***'
-secret_key = '***'
+access_key = 'RQ-0qS4uk-PuRfCiSHyDnfwbS5TPj8vp1fMej5IQ'
+secret_key = 'zaYQPfYpTGklojTBzVh2Vv7S2IQvkipedQCpvGgb'
 
 q = Auth(access_key, secret_key)
 bucket = BucketManager(q)
 
 bucket_name = 'baoer'
 # 前缀
-prefix = None
+prefix = 'user_'
 # 列举条目
 limit = 1000
 # 列举出除'/'的所有文件以及以'/'为分隔的所有前缀
@@ -21,23 +21,16 @@ marker = None
 
 def getItems(marker):
     ret, eof, info = bucket.list(bucket_name, prefix,marker , limit, delimiter)
-    marker = ret["marker"]
-    items = ret["items"]
-    for i in items:
-        print(i["key"].encode("utf-8"))
-    if marker is not None:
+    if ret.has_key('marker'):
+        marker = ret["marker"]
+        items = ret["items"]
+        for i in items:
+            print(i["key"].encode("utf-8"))
         return getItems(marker)
 
 #ret, eof, info = bucket.list(bucket_name, prefix,marker , limit, delimiter)
-
 #market = ret["marker"]
-
 #items = ret["items"]
-
 #for i in items:
 #    print(i["key"])
 getItems(marker)
-assert len(ret.get('items')) is not None
-
-
-
