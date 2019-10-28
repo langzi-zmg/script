79cc170967bce99f3968dcdc11c7e4d15048cfdf
langzi-zmg <zmg_2013@163.com>
2019-10-28 17:54:38 +0800

    Create qiniuGetFileName.py

---
+++ qiniuGetFileName.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from qiniu import Auth
+from qiniu import BucketManager
+import json
+
+access_key = '***'
+secret_key = '***'
+
+q = Auth(access_key, secret_key)
+bucket = BucketManager(q)
+
+bucket_name = 'baoer'
+# 前缀
+prefix = None
+# 列举条目
+limit = 1000
+# 列举出除'/'的所有文件以及以'/'为分隔的所有前缀
+delimiter = None
+# 标记
+marker = None
+
+def getItems(marker):
+    ret, eof, info = bucket.list(bucket_name, prefix,marker , limit, delimiter)
+    marker = ret["marker"]
+    items = ret["items"]
+    for i in items:
+        print(i["key"].encode("utf-8"))
+    if marker is not None:
+        return getItems(marker)
+
+#ret, eof, info = bucket.list(bucket_name, prefix,marker , limit, delimiter)
+
+#market = ret["marker"]
+
+#items = ret["items"]
+
+#for i in items:
+#    print(i["key"])
+getItems(marker)
+assert len(ret.get('items')) is not None
+
+
+
