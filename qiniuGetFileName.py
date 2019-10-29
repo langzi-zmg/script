3ecb521d2cd0b27aa1ec84ce0e54375fa768ec43
langzi-zmg <zmg_2013@163.com>
2019-10-29 09:20:44 +0800

    Update qiniuGetFileName.py

--- qiniuGetFileName.py
+++ qiniuGetFileName.py
@@ -3,15 +3,15 @@ from qiniu import Auth
 from qiniu import BucketManager
 import json
 
-access_key = '**'
-secret_key = '**'
+access_key = '****'
+secret_key = '****'
 
 q = Auth(access_key, secret_key)
 bucket = BucketManager(q)
 
 bucket_name = 'baoer'
 # 前缀
-prefix = 'user_'
+prefix = 'ding'
 # 列举条目
 limit = 1000
 # 列举出除'/'的所有文件以及以'/'为分隔的所有前缀
@@ -21,12 +21,22 @@ marker = None
 
 def getItems(marker):
     ret, eof, info = bucket.list(bucket_name, prefix,marker , limit, delimiter)
-    if ret.has_key('marker'):
+    if type(ret).__name__ == 'dict' and  ret.has_key('marker'):
         marker = ret["marker"]
         items = ret["items"]
         for i in items:
             print(i["key"].encode("utf-8"))
         return getItems(marker)
+    elif type(ret).__name__ != 'dict' :
+        print(eof)
+        print(info)
+        print(ret)
+        return 
+    else:
+        items = ret["items"]
+        for i in items:
+            print(i["key"].encode("utf-8"))
+        return 
 
 #ret, eof, info = bucket.list(bucket_name, prefix,marker , limit, delimiter)
 #market = ret["marker"]
@@ -34,3 +44,4 @@ def getItems(marker):
 #for i in items:
 #    print(i["key"])
 getItems(marker)
+
