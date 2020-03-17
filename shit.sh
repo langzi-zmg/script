300ea420f59a70fed25d18ef882168eb71b91448
zzmg <zmg_2013@163.com>
2020-03-17 11:24:28 +0800

    Delete shit.sh

--- shit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
- coscmd download -r /flume/events/$1/   /data/$1/
-cd /data/$1/
-for i in `ls`;do for j in `ls $i`;do mv -f $i/$j $i/`echo "hour="$j`;done;mv -f $i `echo "dt="$i`;done
-coscmd upload -r /data/$1/  events/ev=$2/
